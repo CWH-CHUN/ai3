392c62ab81a2d95ade755bbbba7c4f0c4347aa79
CWH-CHUN <h098s02923@chunchon.gwe.hs.kr>
2025-11-10 12:17:18 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,10 +74,14 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
     #   "images": ["https://.../jjampong1.jpg", "https://.../jjampong2.jpg"],
     #   "videos": ["https://youtu.be/XXXXXXXXXXX"]
     # },
-    labels[0]:{"texts":["박"]},
-    labels[1]:{"texts":["윤"]},
-    labels[2]:{"texts":["이"]},
-    labels[3]:{"texts":["이"]},
+    labels[0]:{"texts":["박"],
+              "vidioes":[y]},
+    labels[1]:{"texts":["윤"],
+              "vidioes":[kk]},
+    labels[2]:{"texts":["이"],
+"vidioes":[l]},
+    labels[3]:{"texts":["이"],
+"vidioes":[g]},
 }
 
 # ======================
